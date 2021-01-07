8afa2dc792b53de14a607a2ad2a8c0e12b6e84a6
KaneishaR <63017525+KaneishaR@users.noreply.github.com>
2021-01-07 13:17:24 -0600

    Add files via upload

---
+++ positivity-api/src/main/java/com/reesemedia/positivityapi/PositivityApiApplication.java
@@ -0,0 +1,13 @@
+package com.reesemedia.positivityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositivityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositivityApiApplication.class, args);
+	}
+
+}
---
+++ positivity-api/src/test/java/com/reesemedia/positivityapi/PositivityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reesemedia.positivityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositivityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
